a214cc2bc77ede9e191995d483b4c0678c4590c5
SchmittClarisse <82055525+SchmittClarisse@users.noreply.github.com>
2021-07-14 18:03:46 +0200

    exo3

--- exo_01.js
+++ exo_01.js
@@ -4,16 +4,16 @@ const list = [10, 15, 3, 7]
 const k = 17
 
 class FirstExo{
-    constructor(array,k){
-        this.array = array
+    constructor(list,k){
+        this.list = list
         this.k = k 
         this.programm()
     }
     programm(){
         let result = false
-        for(let i = 0; i < this.array.length; i++){
-        for(let j = 1; j < this.array.length; j++){
-            if (this.array[i] + this.array[j] === this.k){
+        for(let i = 0; i < this.list.length; i++){
+        for(let j = 1; j < this.list.length; j++){
+            if (this.list[i] + this.list[j] === this.k){
             return console.log(result = true)}
         }
     }
